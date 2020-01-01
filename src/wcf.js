f26d66deafcefb77b01d8ea17b563cbdcba7b053
kmmiles <kenneth.mmiles@gmail.com>
2020-01-01 05:33:56 -0800

    vengeance bug in balorDPS

--- src/wcf.js
+++ src/wcf.js
@@ -539,7 +539,7 @@ var wcf = {
         (0.83 + spellHit / 100) *
         (this.moonFuryBonus(moonFuryPoints) * spellBaseDamage +
           spellCoefficient * spellPower) *
-        (1 + ((wcf.globals.critMultiplier - 1) * spellCrit) / 100)) /
+        (1 + ((this.spellCritBonus(vengeancePoints) - 1) * spellCrit) / 100)) /
       (spellCastTime -
         this.naturesGraceBonus(naturesGrace) *
           (0.83 + spellHit / 100) *
